7d0e66a46ed01ecd0132482e2d8eafb8579e7d3c
C846279 <Chhitij.Bhattachan@BNSF.com>
2018-09-29 21:10:57 -0500

    new project

---
+++ src/com/chhitijbhattachan/GrandChild.java
@@ -0,0 +1,11 @@
+package com.chhitijbhattachan;
+
+public class GrandChild {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Parent parent = new Parent();
+		System.out.println(parent.speed);
+	}
+
+}
